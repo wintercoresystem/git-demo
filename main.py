e491ff3b19c3c59500a6849ff395e68829d879fa
wintercoresystem <wintercoresystem@gmail.com>
2023-11-25 13:39:37 +0300

    Now you can use command line arguments

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def main():
-    print("Hello world")
+import sys
 
+def main():
+    print("Hello", end=" ")
+    for i in sys.argv[1:len(sys.argv)]:
+        print(i, end=", ")
+    print()
+    
 if __name__ == "__main__":
     main()
