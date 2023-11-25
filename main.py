4e4ebab4625807ef15dc56a5439e3e8104e10cae
wintercoresystem <wintercoresystem@gmail.com>
2023-11-25 13:52:43 +0300

    Added comprehensive comments

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import sys
 
+# Unnecessary commentaries for this code
 def main():
-    print("Hello", end=" ")
+    """
+    Main function to look proffesional
+    Prints "Hello" + every command like argument you entered
+    """
+    print("Hello", end=" ") # printing first word, which is "Hello"
+
+    # For every system line argument it will type argument + ", "
     for i in sys.argv[1:len(sys.argv)]:
         print(i, end=", ")
-    print()
+    # Empty print for termianl not to look ugly.
+    print() 
     
 if __name__ == "__main__":
     main()
